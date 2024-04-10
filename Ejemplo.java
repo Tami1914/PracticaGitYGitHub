9d91c95a8c16d39eea7ca910beecadf9ee762397
Tami1914 <aythami.1914@gmail.com>
2024-04-10 16:16:14 +0100

    Cambios realizados

--- Ejemplo.java
+++ Ejemplo.java
@@ -1,11 +1,17 @@
 public class Ejemplo{
   private int x;
+  private int y;
 
-  public Ejemplo(int x){
+  public Ejemplo(int x, int y){
     this.x = x;
+    this.y = y;
   }
 
   public int getX(){
     return x;
+  }
+
+  public int getY(){
+    return y;
   }
 }
